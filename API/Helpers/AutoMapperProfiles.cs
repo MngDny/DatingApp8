fbbb80c68681183bc9321631ee0e5e0be9b7d177
MngDny <mngdny@gmail.com>
2024-05-29 12:32:11 +0300

    End of section 10

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -3,6 +3,7 @@ using API.Entities;
 using API.Extensions;
 using AutoMapper;
 
+
 namespace API.Helpers
 {
     public class AutoMapperProfiles : Profile
@@ -13,6 +14,8 @@ namespace API.Helpers
                 .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
                 .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo,PhotoDto>();
+
+            CreateMap<MemberUpdateDto, AppUser>();
         }
     }
 }
